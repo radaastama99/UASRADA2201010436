093bc3eeb1871c0b3e153ea7eb0ca7895b0cdb1d
radaastama99 <117253500+radaastama99@users.noreply.github.com>
2023-01-24 20:23:02 +0700

    Add files via upload

---
+++ slide/script.js
@@ -0,0 +1,11 @@
+
+function simpan(){
+    var nama = document.getElementById('nama').value;
+    var email = document.getElementById('email').value;
+    var umur = document.getElementById('umur').value;
+    var gender = document.getElementById('jeniskelamin').value;
+    var saran = document.getElementById('saran').value;
+
+    alert ("Data Berhasil Disimpan atas " + "Nama : "+ nama)
+}
+
